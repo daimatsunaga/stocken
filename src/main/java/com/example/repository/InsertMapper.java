9b0488083aeaebddb7fa81d3bc5106fe860999de
daimatsunaga <dai.matsunaga@rakus-partners.co.jp>
2022-02-17 10:02:42 +0900

    不要コード削除

--- src/main/java/com/example/repository/InsertMapper.java
+++ src/main/java/com/example/repository/InsertMapper.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
-import com.example.domain.Category;
 import com.example.domain.Original;
 
 @Mapper
@@ -14,9 +13,7 @@ public interface InsertMapper {
 	public void insertOriginal(@Param("originalList")List<Original> originalList);
 	
 	public List<String> findAllOriginal();
-	
-	public void insertCategory(@Param("categoryList")List<Category> category);
-	
+		
 	public void insertCategorySize1(@Param("parent")Integer parent, @Param("category")String category, @Param("nameAll") String nameAll);
 	public void insertCategorySize2(@Param("parent")Integer parent, @Param("category")List<String> category, @Param("nameAll") String nameAll);
 	public void insertCategorySize3(@Param("parent")Integer parent, @Param("category")List<String> category, @Param("nameAll") String nameAll);
@@ -26,11 +23,5 @@ public interface InsertMapper {
 	public Integer countLows();
 	
 	public String findById(@Param("id")Integer id);
-	
-	public void insertFirst(@Param("name")String name);
-	
-	public void insertSecond(@Param("parent")Integer parent, @Param("name")String name);
-	
-	public void insertThird(@Param("parent")Integer parent, @Param("name")String name, @Param("nameAll") String nameAll);
 	
 }
